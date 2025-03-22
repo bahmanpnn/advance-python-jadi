1241411ab36ec868b9b150ba1156631569e5e52d
Bahman Pournazari <bahmanpn@gmail.com>
2025-03-23 00:45:55 +0330

    4_1 and 4_2 practicing file added.there are some examples for regex in python and use some methods of re module

--- 4/4_1and2_regex.py
+++ 4/4_1and2_regex.py
@@ -30,4 +30,100 @@
                                 if a name be more than 3 part regex just get first three parts and words
     
     "^\w+\W+"gm
+
+
+    ############
+    ep 2
+    package of regex is re and have some methods that we can use.==> re=regular expression
+    search / match / findall / sub
+    span
+    start
+    end
+    ...
+
+
+    
+    [sS][aA][lL][aA][mM] (\w+)[\.]?
+    [sS][aA][lL][aA][mM]\s(.+?)[\.]
+    .+@.+\..{2,3}
+    .+\@.+\..{2,3} ==>i dont know why jadi used \ for @ ==>\@ :///
+
 """
+
+import re
+
+
+# example 1
+str="salam bahman.salam amir.Salam asghar. SALAM haji.saLam dadash.salam sotoon"
+
+result=re.search(r'[sS][aA][lL][aA][mM]\s(.+?)[\.]',str)
+result2=re.match(r'[sS][aA][lL][aA][mM]\s(.+?)[\.]',str)
+print(result)
+print(result2)
+print("_"*50)
+print(result.start())
+print(result.end())
+print(result.span())
+
+
+
+print("_"*50)
+# example 2
+email="bahmanpn@gmail.com"
+
+
+result=re.search(r'.+@.+\..{2,3}',email)
+if result== None:
+    print("email input is not correct!!")
+else:
+    print(result.group())
+
+
+
+print("_"*50)
+# example 3
+str='''
+    the price of oil is 65$ for 3 boshke for yesterday
+    the price of oil is 78$ for 3 boshke for today
+    the price of oil is 61$ for 3 boshke for tomorrow 23-03
+    the price of oil is 63$ for 3 boshke for 24/03/2025
+'''
+
+# res=re.findall(r'the price of oil is (\d+)\$ for (\d+\s)boshke for (.*)',str)
+res=re.findall(r'the price of oil is (\d+)\$ for (\d+) boshke for (.*)',str)
+print(res)
+print(res[0])
+
+print("="*25)
+for price,boshke_count,day in res:
+    print(price,boshke_count,day)
+
+
+
+# example 4 sub
+print("_"*50)
+str="salam bahman.salam amir.Salam asghar. SALAM haji.saLam dadash.salam sotoon"
+# print(re.sub('','',str))
+# print(re.sub('salam','Hi',str))
+# print(re.sub('[sS][aA][lL][aA][mM]','Hi',str))
+# print(re.sub('[sS][aA][lL][aA][mM]','Hi',str))
+# print(re.sub('([sS][aA][lL][aA][mM])','Hi \g<0>',str)) # all str
+# print(re.sub('([sS][aA][lL][aA][mM])','Hi \g<1>',str)) # ==>first group
+print(re.sub('[sS][aA][lL][aA][mM] (\w+)\.','Hi \g<1>.',str)) # ==>first group ==> Hi+(\w+).
+print(re.sub('[sS][aA][lL][aA][mM] (\w+)','Hi \g<1>',str)) # ==>first group ==> Hi+(\w+)
+
+
+
+
+print("_"*50)
+# example 5 ****
+str2='''
+    the price of oil is 65$ for 3 boshke for yesterday
+    the price of oil is 78$ for 3 boshke for today
+    the price of oil is 61$ for 3 boshke for tomorrow 23-03
+    the price of oil is 63$ for 3 boshke for 24/03/2025
+'''
+
+replace_str="the price of oil is (\d+)\$ for (\d+) boshke for (.*)"
+res2=re.sub(replace_str,'day: \g<3> -price: \g<1> dollars - \g<2> boshke',str2)
+print(res2)
